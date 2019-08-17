19634c7fa1079f44f5cca9778d893d03e7303110
Ganesh <say.ganeshramyadav@gmail.com>
2019-08-17 16:16:26 +0530

    Will update the key generator function

--- src/StackUtil/Utils.php
+++ src/StackUtil/Utils.php
@@ -7,15 +7,11 @@ class Utility
 {    
 	const idLength = 20;
 	const keySplitlength = 4;
-	const keyLength =16;
+	const keyLength = 12;
 	const idStr = "1234567890abcdefghijklmnopqrstuvwxyz";
-	const keyStr = "1234567890";
+    const keyStr = "1234567890123456789012345678901234567890";
+    const splitChar = '-';
 
-    public static function helloworld()
-	{
-		return 'HelloWorld';
-	}
-	
 	public static function generateId($firstChar, $shortName){
 		$id = substr(str_shuffle(self::idStr),0,self::idLength);
 		$id = $firstChar.$shortName.$id;
@@ -23,7 +19,13 @@ class Utility
    }
 
 	public static function generateKey($shortName){
-		return $shortName;
+		$key = substr(str_shuffle(self::keyStr),0,self::keyLength);
+		$generatedKey = strtoUpper($shortName);
+		for($i = 0;$i < self::keyLength; $i = $i + self::keySplitlength){
+			$keyString = substr($key, $i, self::keySplitlength);
+			$generatedKey = $generatedKey.self::splitChar.$keyString;
+		}
+		return $generatedKey;
 	}
 
 	public static function objArraySearch($array, $index, $value)
